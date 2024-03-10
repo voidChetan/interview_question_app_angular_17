471ef8edc28a9277e513d9dc7a6db31f95d23017
codefirst2022 <codefirst2022@gmail.com>
2024-03-10 20:09:38 +0530

    changes

--- src/app/pages/question-count/question-count.component.ts
+++ src/app/pages/question-count/question-count.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-question-count',
@@ -8,7 +8,15 @@ import { Component, Input } from '@angular/core';
   templateUrl: './question-count.component.html',
   styleUrl: './question-count.component.css'
 })
-export class QuestionCountComponent {
+export class QuestionCountComponent implements OnChanges {
   @Input() languageCountList: any [] = [];
+
+  loader: boolean = true;
+   
+  ngOnChanges(changes: SimpleChanges): void {
+    setTimeout(() => {
+      this.loader = false;
+    }, 2000);
+  }
 
 }
